0398df6698cfce60cb6530687712f62b9fd8b34d
Purvi <pandyapurvi134@yahoo.in>
2019-06-23 23:16:41 +1000

    Updates

---
+++ precache-manifest.522e00aba0207a3d3a717a285ae72b45.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "288ab49d7a271188f73fc069c2e2b6a1",
+    "url": "/deploy/index.html"
+  },
+  {
+    "revision": "7bfd73e3e0ecfd85478f",
+    "url": "/deploy/static/css/main.e6354961.chunk.css"
+  },
+  {
+    "revision": "6cfae57cb4c4261312ee",
+    "url": "/deploy/static/js/2.cec531a9.chunk.js"
+  },
+  {
+    "revision": "7bfd73e3e0ecfd85478f",
+    "url": "/deploy/static/js/main.853c053d.chunk.js"
+  },
+  {
+    "revision": "0dcb4eec97520abc4f58",
+    "url": "/deploy/static/js/runtime~main.9c98c2a1.js"
+  }
+]);
